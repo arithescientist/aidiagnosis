d3c976c77a05501e47a9b7bfe45b1a23c41fd2ae
Ari Iwunze <62076939+ariavathlete@users.noreply.github.com>
2020-10-02 14:54:40 -0500

    Add files via upload

--- app/views/main.py
+++ app/views/main.py
@@ -2,42 +2,69 @@ from flask import render_template, jsonify, Flask, redirect, url_for, request
 from app import app
 import random
 import os
-from keras.applications.resnet50 import ResNet50
+import cv2
+from tensorflow.keras.preprocessing.image import load_img
+from keras.applications.vgg16 import VGG16
 from keras.preprocessing import image
-from keras.applications.resnet50 import preprocess_input, decode_predictions
+from keras.applications.vgg16 import preprocess_input, decode_predictions
 import numpy as np
+from tensorflow.keras.models import load_model
 
 
 @app.route('/')
-
-#disease_list = ['Atelectasis', 'Consolidation', 'Infiltration', 'Pneumothorax', 'Edema', 'Emphysema', \
-                  # 'Fibrosis', 'Effusion', 'Pneumonia', 'Pleural_Thickening', 'Cardiomegaly', 'Nodule', 'Mass', \
-                  # 'Hernia']
-
-@app.route('/upload')
-def upload_file2():
-   return render_template('index.html')
-	
-@app.route('/uploaded', methods = ['GET', 'POST'])
-def upload_file():
-   if request.method == 'POST':
-      f = request.files['file']
-      path = os.path.join(app.config['UPLOAD_FOLDER'], f.filename)
-      model= ResNet50(weights='imagenet')
-      img = image.load_img(path, target_size=(224,224))
-      x = image.img_to_array(img)
-      x = np.expand_dims(x, axis=0)
-      x = preprocess_input(x)
-      preds = model.predict(x)
-      preds_decoded = decode_predictions(preds, top=3)[0] 
-      print(decode_predictions(preds, top=3)[0])
-      f.save(path)
-      return render_template('uploaded.html', title='Success', predictions=preds_decoded, user_image=f.filename)
-
-
 @app.route('/index')
 def index():
     return render_template('index.html', title='Home')
+
+@app.route('/upload.html')
+def upload():
+   return render_template('upload.html')
+
+@app.route('/upload_chest.html')
+def upload_chest():
+   return render_template('upload_chest.html')
+
+@app.route('/uploaded_chest', methods = ['GET', 'POST'])
+def uploaded_chest():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file:
+            # filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], 'upload_chest.jpg'))
+    model = VGG16(weights='imagenet',  include_top = False)
+                      
+    image = cv2.imread('./app/forms/upload_chest.jpg') # read file 
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # arrange format as per keras
+    image = cv2.resize(image,(224,224))
+    image = np.array(image) / 255
+    image = np.expand_dims(image, axis=0)
+    x = preprocess_input(image)
+                      
+    preds = model.predict(x)
+    preds_decoded = decode_predictions(preds, top=3)[0]
+                      
+    print(preds_decoded)
+    f.save(path)
+                      
+    if preds_decoded[0] > 0.5:
+        predss = str('%.2f' % (preds_decoded[0]*100) + '% Pneumonia') 
+    else:
+        predss = str('%.2f' % ((1-preds_decoded[0])*100) + '% Normal')
+    print(predss)
+    return render_template('results_chest.html', title='Success', predictions=preds_decoded)
+
+
+
+
 
 @app.route('/map')
 def map():
@@ -46,12 +73,16 @@ def map():
 
 @app.route('/map/refresh', methods=['POST'])
 def map_refresh():
-    points = [(random.seed(41.873937, 41.868075),
-               random.seed(-87.646826, -87.626410))
-              for _ in range(random.seed(2, 3))]
+    points = [(random.seed(41.873937, 41.868075), random.seed(-87.646826, -87.626410))
+              for _ in range(random.seed(2, 9))]
     return jsonify({'points': points})
 
 
 @app.route('/contact')
 def contact():
     return render_template('contact.html', title='Contact')
+
+@app.route('/demo')
+def demo():
+    return render_template('demo.html', title='Demo')
+
