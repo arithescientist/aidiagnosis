038dbca16762ba676c953d9295277493d476eb34
Ari Iwunze <62076939+ariavathlete@users.noreply.github.com>
2020-10-21 15:38:53 -0500

    Add files via upload

--- app/config_prod.py
+++ app/config_prod.py
@@ -17,13 +17,20 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 # Configuration of a Gmail account for sending mails
-MAIL_SERVER = 'smtp.googlemail.com'
-MAIL_PORT = 465
+import os
+
+
+db_user = os.environ.get('DB_USER')
+db_password = os.environ.get('DB_PASS')
+
+
+MAIL_SERVER = 'smtp.gmail.com'
+MAIL_PORT = 587
 MAIL_USE_TLS = False
 MAIL_USE_SSL = True
-MAIL_USERNAME = 'flask.boilerplate'
-MAIL_PASSWORD = 'flaskboilerplate123'
-ADMINS = ['flask.boilerplate@gmail.com']
+MAIL_USERNAME = 'iDiagnosis'
+MAIL_PASSWORD = db_password
+ADMINS = [db_user]
 
 # Number of times a password is hashed
 BCRYPT_LOG_ROUNDS = 12
